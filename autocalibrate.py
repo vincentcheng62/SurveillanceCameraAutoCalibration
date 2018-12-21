4aabbec74b1cfb803bd86dce4e250b525b944b90
VIncent Cheng <vincentcheng62@gmail.com>
2018-12-21 09:34:07 +0800

    Integrate the gps calculation into it

--- autocalibrate.py
+++ autocalibrate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import cv2
 import time, datetime
 from matplotlib import pyplot as plt
 import operator
@@ -13,8 +14,11 @@ from imutils.object_detection import non_max_suppression
 from numpy.linalg import inv
 from math import log10, floor
 
+import pyproj
+
 IsEachFrameDebug = False
 hog_threshold = 0.9
+regionpts = np.array([[361,195],[414,195],[558,691],[761,685]], np.int32)
 
 def inside(r, q):
     rx, ry, rw, rh = r
@@ -81,51 +85,84 @@ def PrintLocalization(Lmap, bigger_frame, pick, ratio, Size_of_w, physical_size,
         bigger_frame_reproject = cv.circle(bigger_frame_reproject, (int(bottom_pt[0]), int(bottom_pt[1])), 5, (0,255,0), thickness=3, lineType=8, shift=0) 
         bigger_frame_reproject = cv.circle(bigger_frame_reproject, (int(head_pt[0]), int(head_pt[1])), 5, (0,255,0), thickness=3, lineType=8, shift=0) 
 
+        print("head_pt: ", head_pt)
         print("bottom_pt: ", bottom_pt)
         bp = np.zeros((1, 2), np.float32)
         bp[0][0] = bottom_pt[0]
         bp[0][1] = bottom_pt[1]
+        hp = np.zeros((1, 2), np.float32)
+        hp[0][0] = head_pt[0]
+        hp[0][1] = head_pt[1]        
         #print("calib_corners: ", calib_corners[0])
         dst = cv.undistortPoints(bp.reshape(-1,1,2).astype(np.float32), camera_matrix_manual, dist_coefs_manual)
         bottom_pt = dst[0][0]
+        dst2 = cv.undistortPoints(hp.reshape(-1,1,2).astype(np.float32), camera_matrix_manual, dist_coefs_manual)        
+        head_pt = dst2[0][0]
         # according to the formula: s1=1.7*r33+s2, s2=1.7*(r23-v1*r33)/(v1-v2)
         #s2 = 1.7*(rot[1][2]-head_pt[1]*rot[2][2])/(head_pt[1]-bottom_pt[1])
+        print("head_pt after undistort: ", head_pt)
         print("bottom_pt after undistort: ", bottom_pt)
         #bottom_pt_center_normalized = (bottom_pt[0]-camera_matrix_manual[0][2], bottom_pt[1]-camera_matrix_manual[1][2])
         #print("bottom_pt_center_normalized: ", bottom_pt_center_normalized)        
         #s1 = 1.7*rot[2][2]+s2
 
         #print("ref_tvec: ", ref_tvec[0][0], ref_tvec[1][0], ref_tvec[2][0])
-        Tz = rot[0][2]*ref_tvec[0][0]+rot[1][2]*ref_tvec[1][0]+rot[2][2]*ref_tvec[2][0]
-        dz = rot[0][2]*bottom_pt[0]+rot[1][2]*bottom_pt[1]+rot[2][2]*1
-        s2 = Tz/dz
-        print("s2: ", s2)
+        #AlgMethod=1  # 0 : assume z=0, 1: assume height=1.7
+        s1g, s2g = GetBottomPtScale(head_pt, bottom_pt, rot, ref_tvec, 0)
+        s1, s2 = GetBottomPtScale(head_pt, bottom_pt, rot, ref_tvec, 1)
 
-        img_pt = np.array([[bottom_pt[0]], [bottom_pt[1]], [1]])
+        img_pt_h = np.array([[head_pt[0]], [head_pt[1]], [1]])
+        img_pt_b = np.array([[bottom_pt[0]], [bottom_pt[1]], [1]])
         #print("zz: ", s2*img_pt-ref_tvec)
         #print("tra: ", cv.transpose(rot))
-        result = np.matmul(cv.transpose(rot), s2*img_pt-ref_tvec)
-        print("result: ",cv.transpose(result))
+        result1 = np.matmul(cv.transpose(rot), s1*img_pt_h-ref_tvec)
+        result2 = np.matmul(cv.transpose(rot), s2*img_pt_b-ref_tvec)
+        resultg = np.matmul(cv.transpose(rot), s2g*img_pt_b-ref_tvec)
+        print("result1: ",cv.transpose(result1))
+        print("result2: ",cv.transpose(result2))
+        print("resultg: ",cv.transpose(resultg))
+        print("resultg-result2: ",cv.transpose(resultg-result2))
 
-        center = (int(Size_of_w*0.5-result[1][0]*px_of_meter), int(result[0][0]*px_of_meter+Size_of_w*0.5))     
-        print("world XY: ", result[0][0], result[1][0])    
-        print("window XY: ", center[0], center[1])
+        center = (int(Size_of_w*0.5-result2[1][0]*px_of_meter), int(result2[0][0]*px_of_meter+Size_of_w*0.5))     
+        #print("world XYZ: ", result2[0][0], result2[1][0], result2[2][0])    
+        print("window XY of bottom pt: ", center[0], center[1])
         #print("result: ", result)
-        pchar = "[" + str(round_to_1(result[0][0])) + ", " + str(round_to_1(result[1][0])) + "]"
+        pchar = "[" + str(round_to_1(result2[0][0])) + ", " + str(round_to_1(result2[1][0])) + ", " + str(round_to_1(result2[2][0])) + "]"
 
         Lmap_localized = cv.circle(Lmap_localized, center, 5, (255,0,0), thickness=3, lineType=8, shift=0) 
         cv.putText(Lmap_localized, pchar, center, cv.FONT_HERSHEY_COMPLEX, 0.5, (0,255,0), 1) 
 
         #Project the resulting 3d point onto 2d image again for comfirmation.
-        imgpts, jac = cv.projectPoints(np.array([[result[0][0], result[1][0], result[2][0]], [0.0, 0.0, 0.0]]), ref_rvec, ref_tvec, camera_matrix_manual, dist_coefs_manual)
+        imgpts, jac = cv.projectPoints(np.array([[result2[0][0], result2[1][0], result2[2][0]], [0.0, 0.0, 0.0]]), ref_rvec, ref_tvec, camera_matrix_manual, dist_coefs_manual)
         intpt =  (int(imgpts[0][0][0]), int(imgpts[0][0][1]))
-        print("intpt: ", intpt)
+        print("reproject result2 to 2d plane: ", intpt)
 
         if intpt[0]>0 and intpt[0]<bigger_frame_reproject.shape[1] and intpt[1]>0 and intpt[1]<bigger_frame_reproject.shape[0]:
             bigger_frame_reproject = cv.circle(bigger_frame_reproject, intpt, 5, (255,0,0), thickness=3, lineType=8, shift=0) 
-            cv.putText(bigger_frame_reproject, pchar, (intpt[0], intpt[1]+30), cv.FONT_HERSHEY_COMPLEX, 0.8, (255,0,0), 2)         
+            cv.putText(bigger_frame_reproject, pchar, (intpt[0], intpt[1]+30), cv.FONT_HERSHEY_COMPLEX, 0.8, (255,0,0), 2)  
+        
+
+        # Recover the gps coordinate
+        gps_coord = RecoverGPSCoord(resultg)
+        print("gps_coord: ", gps_coord)
+
+        print("==============================================================================================")       
 
     return Lmap_localized, bigger_frame_reproject
+
+def draw_axis_and_ptgrid(img, corners, imgpts, ptgrid):
+
+    cv.drawChessboardCorners(img, pattern_size, corners, True)
+    corner = tuple(corners[0].ravel())
+    img = cv.line(img, corner, tuple(imgpts[0].ravel()), (0,0,255), 3)
+    img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 3)
+    img = cv.line(img, corner, tuple(imgpts[2].ravel()), (255,0,0), 3)
+
+    # Also draw a point grid in the z-plane for debug purpose
+    for pt in ptgrid:
+        img = cv.circle(img, tuple(pt.ravel()), 4, (0,255,0), thickness=2, lineType=8, shift=0) 
+
+    return img
 
 def create_camera_model(camera_matrix, width, height, scale_focal, draw_frame_axis=True):
     fx = camera_matrix[0,0]
@@ -330,14 +367,56 @@ term = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_COUNT, 30, 0.1)
 FinishCalibration = False
 ref_rvec=None
 ref_tvec=None
+ptgrid = None
+grid = np.float32([[0.5,-0.5,0], [0.5,0,0], [0.5,0.5,0],
+                   [0,-0.5,0], [0,0,0], [0,0.5,0],
+                   [-0.5,-0.5,0], [-0.5,0,0], [-0.5,0.5,0],
+                   [-1,-0.5,0], [-1,0,0], [-1,0.5,0],
+                   [-1.5,-0.5,0], [-1.5,0,0], [-1.5,0.5,0],
+                   [-2,-0.5,0], [-2,0,0], [-2,0.5,0],
+                   [-2.5,-0.5,0], [-2.5,0,0], [-2.5,0.5,0],
+                   [-3,-0.5,0], [-3,0,0], [-3,0.5,0],
+                   [-3.5,-0.5,0], [-3.5,0,0], [-3.5,0.5,0],
+                   [-4,-0.5,0], [-4,0,0], [-4,0.5,0],
+                   [-4.5,-0.5,0], [-4.5,0,0], [-4.5,0.5,0],
+                   [-5,-0.5,0], [-5,0,0], [-5,0.5,0],
+                   [-5.5,-0.5,0], [-5.5,0,0], [-5.5,0.5,0],
+                   [-6,-0.5,0], [-6,0,0], [-6,0.5,0],
+                   [-6.5,-0.5,0], [-6.5,0,0], [-6.5,0.5,0],
+                   [-7,-0.5,0], [-7,0,0], [-7,0.5,0],
+                   [-7.5,-0.5,0], [-7.5,0,0], [-7.5,0.5,0],
+                   [-8,-0.5,0], [-8,0,0], [-8,0.5,0],
+                   [-8.5,-0.5,0], [-8.5,0,0], [-8.5,0.5,0],
+                   [-9,-0.5,0], [-9,0,0], [-9,0.5,0],
+                   [-9.5,-0.5,0], [-9.5,0,0], [-9.5,0.5,0],
+                   [-10,-0.5,0], [-10,0,0], [-10,0.5,0],
+                   [-10.5,-0.5,0], [-10.5,0,0], [-10.5,0.5,0],
+                   [-11,-0.5,0], [-11,0,0], [-11,0.5,0],
+                   [-11.5,-0.5,0], [-11.5,0,0], [-11.5,0.5,0],
+                   [-12,-0.5,0], [-12,0,0], [-12,0.5,0],
+                   [-12.5,-0.5,0], [-12.5,0,0], [-12.5,0.5,0],
+                   [-13,-0.5,0], [-13,0,0], [-13,0.5,0],
+                   [-13.5,-0.5,0], [-13.5,0,0], [-13.5,0.5,0],
+                   [-14,-0.5,0], [-14,0,0], [-14,0.5,0],
+                   [-14.5,-0.5,0], [-14.5,0,0], [-14.5,0.5,0]] ).reshape(-1,3)
 
-def draw(img, corners, imgpts):
-    corner = tuple(corners[0].ravel())
-    img = cv.line(img, corner, tuple(imgpts[0].ravel()), (0,0,255), 3)
-    img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 3)
-    img = cv.line(img, corner, tuple(imgpts[2].ravel()), (255,0,0), 3)
+cb_to_ecef_transform = None
+
+# def draw(img, corners, imgpts):
+#     corner = tuple(corners[0].ravel())
+#     img = cv.line(img, corner, tuple(imgpts[0].ravel()), (0,0,255), 3)
+#     img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 3)
+#     img = cv.line(img, corner, tuple(imgpts[2].ravel()), (255,0,0), 3)
+#     return img
+
+def GetWindowWithAxis(Size_of_w, physical_size):
+    int_size = int(Size_of_w*0.5)
+    center = (int_size, int_size)
+    px_of_meter = int(Size_of_w/physical_size)
+    img = np.zeros((Size_of_w, Size_of_w, 3), np.uint8)
+    img = cv.line(img, center, (center[0], center[1]+px_of_meter), (0,0,255), 3)
+    img = cv.line(img, center, (center[0]-px_of_meter, center[1]), (0,255,0), 3)
     return img
-
 
 #line in form of y=ax+c , so a tuple (a, c)
 #return (IsHavingIntersection, inter_x, inter_y)
@@ -353,13 +432,103 @@ def find_two_line_intersection(line1, line2):
         return (True, (d-c)/(a-b), (a*d-b*c)/(a-b))
 
 
+def GetWindowWithAxis(Size_of_w, physical_size):
+    int_size = int(Size_of_w*0.5)
+    center = (int_size, int_size)
+    px_of_meter = int(Size_of_w/physical_size)
+    img = np.zeros((Size_of_w, Size_of_w, 3), np.uint8)
+    img = cv.line(img, center, (center[0], center[1]+px_of_meter), (0,0,255), 3)
+    img = cv.line(img, center, (center[0]-px_of_meter, center[1]), (0,255,0), 3)
+    return img
+
+def GetBottomPtScale(head_pt, bottom_pt, rot, ref_tvec, method=0):
+    s1=1
+    s2=1
+    r13=rot[0][2]
+    r23=rot[1][2]
+    r33=rot[2][2]
+    t1=ref_tvec[0][0]
+    t2=ref_tvec[1][0]
+    t3=ref_tvec[2][0]
+    u1=head_pt[0]
+    v1=head_pt[1]
+    u2=bottom_pt[0]
+    v2=bottom_pt[1]
+    h=-1.7
+
+    if method==0:
+        # Assume the world coord of the bottom pt (X, Y, Z=0), then there is unique solution for s2
+        Tz = r13*t1+r23*t2+r33*t3
+        dz = r13*u2+r23*v2+r33*1.0
+        s2 = Tz/dz
+        print("s2g: ", s2)
+
+    else:
+        # Use head-pt and bottom-pt to approximate the (X, Y, Z) of the bottom pt in which Z != 0
+        # Need to minimize f where f(s1, s2) = Norm((s1u1-s2u2-hr13, s1v1-s2v2-hr23, s1-s2-hr33))
+        # That means, want to find a pair of (s1, s2) such that their world coord (X1, Y1, Z1), (X2, Y2, Z2)
+        # can achieve X1 close to X2, Y1 close to Y2, Z1-Z2 close to h=1.7m normal human height
+        # Taking df/ds1 and df/ds2=0 and solve for s1 and s2
+        # Since u1=u2,...
+
+
+        Df = np.zeros((2, 2), np.float32)
+        Df[0, 0] = 2*(u1*u1+v1*v1+1)
+        Df[0, 1] = -2*(u1*u1+v1*v2+1)
+        Df[1, 0] = -2*(u1*u1+v1*v2+1)
+        Df[1, 1] = 2*(u1*u1+v2*v2+1)
+        #print("Df: ", Df)
+
+        Zero = np.zeros((2, 1), np.float32)
+        Zero[0, 0] = 2*h*(u1*r13+v1*r23+r33)
+        Zero[1, 0] = -2*h*(u1*r13+v2*r23+r33)
+        #print("Zero: ", Zero)
+
+        Answer = np.matmul(inv(Df), Zero)
+        #print("Answer: ", Answer)
+        s1=Answer[0][0]
+        s2=Answer[1][0]
+
+        #Calculate the loss vector
+        rhs = np.zeros((3, 1), np.float32)
+        rhs[0, 0] = h*r13
+        rhs[1, 0] = h*r23
+        rhs[2, 0] = h*r33
+
+        lhs = np.zeros((3, 1), np.float32)
+        lhs[0, 0] = s1*u1-s2*u2
+        lhs[1, 0] = s1*v1-s2*v2
+        lhs[2, 0] = s1-s2
+
+        #print("loss vector: ", lhs-rhs)    
+        diff = np.matmul(inv(rot), lhs)
+        print("diff vector: ", cv.transpose(diff))    
+        print("s1: ", s1, ", s2: ", s2)
+
+    return s1, s2
+
+def RecoverGPSCoord(cb_coord):
+    ecef_coord = np.matmul(cb_to_ecef_transform[:,0:3], cb_coord) + cv.transpose(np.array([cb_to_ecef_transform[:, 3]]))
+    print("ecef_coord: ", cv.transpose(ecef_coord))
+
+    ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+    lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+    lon, lat, alt = pyproj.transform(ecef, lla, ecef_coord[0][0], ecef_coord[1][0], ecef_coord[2][0], radians=False)
+    return (lat, lon, alt)
+
 calib_corners = None
 calib_imgpt = None
 axis = np.float32([[0.5,0,0], [0,0.5,0], [0,0,-0.5]]).reshape(-1,3)
 frame_num=0
 hog = cv.HOGDescriptor()
 hog.setSVMDetector( cv.HOGDescriptor_getDefaultPeopleDetector() )
-while(cap.isOpened() and len(line_db)<line_db_need_to_collect):
+
+fs_read = cv2.FileStorage("cb_to_ecef.yml", cv2.FILE_STORAGE_READ)
+cb_to_ecef_transform = fs_read.getNode("transform").mat()
+print("cb_to_ecef_transform: ", cb_to_ecef_transform)
+fs_read.release()   
+
+while(cap.isOpened()):
     frame_num = frame_num+1
     start = time.time()
     ret, frame = cap.read()
@@ -491,7 +660,9 @@ while(cap.isOpened() and len(line_db)<line_db_need_to_collect):
                 imgpts2, jac2 = cv.projectPoints(axis, rvecs, tvecs, camera_matrix_manual, dist_coefs_manual)
                 calib_corners = corners
                 calib_imgpt = imgpts2
-                frame = draw(frame,calib_corners,calib_imgpt)
+                imgpts3, jac3 = cv.projectPoints(grid, rvecs, tvecs, camera_matrix_manual, dist_coefs_manual)
+                ptgrid = imgpts3
+                #frame = draw(frame,calib_corners,calib_imgpt)
 
                 imgpts, jac = cv.projectPoints(np.array(obj_points), rvecs, tvecs, camera_matrix_manual, dist_coefs_manual)
                 #print(imgpts[0][0])
@@ -583,8 +754,11 @@ while(cap.isOpened() and len(line_db)<line_db_need_to_collect):
             # plt.show()       
 
     ratio = 2.0
+    ratioint = int(ratio)
+    pick_inregion=[]
     if frame.any() and FinishCalibration:
-        frame = draw(frame,calib_corners,calib_imgpt)
+        frame_display = frame.copy()
+        frame_display = draw_axis_and_ptgrid(frame_display,calib_corners,calib_imgpt, ptgrid)
         resized_frame = cv.resize(frame, (0,0), fx=(1/ratio), fy=(1/ratio)) 
         rects, weight = hog.detectMultiScale(resized_frame, winStride=(8, 8), padding=(32,32), scale=1.05)
         
@@ -603,19 +777,23 @@ while(cap.isOpened() and len(line_db)<line_db_need_to_collect):
         #         found_filtered.append(r)
         # draw_detections(resized_frame, found_filtered, 3)
         found_filtered = np.array([[x, y, x + w, y + h] for (x, y, w, h) in found_filtered])
-        pick = non_max_suppression(found_filtered, probs=None, overlapThresh=0.65)
+        pick = non_max_suppression(found_filtered, probs=None, overlapThresh=0.3)
 
         # draw the final bounding boxes
         for (xA, yA, xB, yB) in pick:
-            cv.rectangle(resized_frame, (xA, yA), (xB, yB), (0, 255, 0), 2)        
+            ground_center = ((xA+xB)*0.5*ratio, max(yA, yB)*ratio)
+            dist = cv2.pointPolygonTest(regionpts,ground_center,True)
+            if not dist < -1 :
+                pick_inregion.append((xA*ratioint, yA*ratioint, xB*ratioint, yB*ratioint))
+                cv.rectangle(frame_display, (xA*ratioint, yA*ratioint), (xB*ratioint, yB*ratioint), (0, 255, 0), 2)        
 
-        bigger_frame = cv.resize(resized_frame, (0,0), fx=ratio, fy=ratio) 
+        #bigger_frame = cv.resize(resized_frame, (0,0), fx=ratio, fy=ratio) 
         #cv.imshow('pedestrian detection', bigger_frame)        
 
         Size_of_w=600
-        physical_size=20
+        physical_size=30
         Lmap = GetWindowWithAxis(Size_of_w, physical_size)
-        Lmap_localized, bigger_frame_reproject = PrintLocalization(Lmap, bigger_frame, pick, ratio, Size_of_w, physical_size, camera_matrix_manual, dist_coefs_manual, ref_rvec, ref_tvec, calib_corners)
+        Lmap_localized, bigger_frame_reproject = PrintLocalization(Lmap, frame_display, pick_inregion, 1.0, Size_of_w, physical_size, camera_matrix_manual, dist_coefs_manual, ref_rvec, ref_tvec, calib_corners)
 
         cv.imshow('pedestrian detection', bigger_frame_reproject)        
         cv.imshow('localization', Lmap_localized)      
@@ -632,21 +810,21 @@ while(cap.isOpened() and len(line_db)<line_db_need_to_collect):
 cap.release()
 cv.destroyAllWindows()
 
-print("Collected over {} major axis of pedestrian blob, start calculation...".format(line_db_need_to_collect))
+# print("Collected over {} major axis of pedestrian blob, start calculation...".format(line_db_need_to_collect))
 
-#Allocate the voting space
-voting_space={}
-for line1 in line_db:
-    for line2 in line_db:
-        ret = find_two_line_intersection(line1, line2)
-        if ret[0] == True:
-            int_coord = (int(ret[1]), int(ret[2]))
-            if int_coord in voting_space:
-                voting_space[int_coord] = voting_space[int_coord]+1
-            else:
-                voting_space[int_coord] = 1
+# #Allocate the voting space
+# voting_space={}
+# for line1 in line_db:
+#     for line2 in line_db:
+#         ret = find_two_line_intersection(line1, line2)
+#         if ret[0] == True:
+#             int_coord = (int(ret[1]), int(ret[2]))
+#             if int_coord in voting_space:
+#                 voting_space[int_coord] = voting_space[int_coord]+1
+#             else:
+#                 voting_space[int_coord] = 1
 
-if voting_space:
-    vote_coord = max(voting_space.items(), key=operator.itemgetter(1))[0]
-    print("Finish voting the pixel level vanishing point, which is {}, have {} vote".format(vote_coord, voting_space[vote_coord]))
+# if voting_space:
+#     vote_coord = max(voting_space.items(), key=operator.itemgetter(1))[0]
+#     print("Finish voting the pixel level vanishing point, which is {}, have {} vote".format(vote_coord, voting_space[vote_coord]))
 
