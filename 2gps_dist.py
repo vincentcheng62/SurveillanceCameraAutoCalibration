15f193cf6ca68f92ea5f75a6ffed08651e33a7a1
VIncent Cheng <vincentcheng62@gmail.com>
2019-01-03 11:03:37 +0800

    A simple script to measure 2 gps pt dist

---
+++ 2gps_dist.py
@@ -0,0 +1,32 @@
+import cv2
+import math
+fs_read = cv2.FileStorage("twogps.yml", cv2.FILE_STORAGE_READ)
+lat1 = fs_read.getNode("lat1").real()
+lon1 = fs_read.getNode("lon1").real()
+print("lat1, lon1: ", lat1, ", ", lon1)
+lat2 = fs_read.getNode("lat2").real()
+lon2 = fs_read.getNode("lon2").real()
+print("lat2, lon2: ", lat2, ", ", lon2)
+fs_read.release()
+
+
+def degreesToRadians(degrees):
+  return degrees * math.pi / 180
+
+earthRadiusKm = 6371;
+
+dLat = degreesToRadians(lat2-lat1);
+dLon = degreesToRadians(lon2-lon1);
+
+lat1 = degreesToRadians(lat1);
+lat2 = degreesToRadians(lat2);
+
+a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2); 
+c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a)); 
+dist = earthRadiusKm * c *1000.0
+print("dist: ", dist, "m")
+
+
+
+
+
