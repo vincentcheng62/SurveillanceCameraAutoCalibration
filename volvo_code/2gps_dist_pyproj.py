0c36a83e9c207daa9586f59cba912f48e0a438bf
VIncent Cheng <vincentcheng62@gmail.com>
2019-08-27 10:12:25 +0800

    Add files via upload

---
+++ volvo_code/2gps_dist_pyproj.py
@@ -0,0 +1,23 @@
+import cv2
+import math
+
+fs_read = cv2.FileStorage("twogps.yml", cv2.FILE_STORAGE_READ)
+lat1 = fs_read.getNode("lat1").real()
+lon1 = fs_read.getNode("lon1").real()
+print("lat1, lon1: ", lat1, ", ", lon1)
+lat2 = fs_read.getNode("lat2").real()
+lon2 = fs_read.getNode("lon2").real()
+print("lat2, lon2: ", lat2, ", ", lon2)
+fs_read.release()
+
+_GEOD = pyproj.Geod(ellps='WGS84')
+a,a2,d = _GEOD.inv(lon1,lat1,lon2,lat2) 
+print("pyproj   dist:", d, "m")
+
+
+
+
+
+
+
+
